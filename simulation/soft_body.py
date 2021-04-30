d617402199a899ea547a4e8f51eaf2024d52087a
mehdi <mehdidolati@hotmail.com>
2021-05-01 01:29:08 +0430

    polygon collision

--- simulation/soft_body.py
+++ simulation/soft_body.py
@@ -3,6 +3,47 @@ import numpy as np
 import networkx as nx
 
 
+def to_coord(v):
+    return v[0] * RIGHT + v[1] * UP
+
+
+class Polygon:
+    def __init__(self):
+        self.shape = nx.Graph()
+
+    def add_edge(self, x, y):
+        if x not in self.shape.nodes():
+            self.shape.add_node(x)
+        if y not in self.shape.nodes():
+            self.shape.add_node(y)
+        if (x, y) not in self.shape.edges():
+            self.shape.add_edge(x, y, q=set())
+
+    def add_edges(self, edge_list):
+        for e in edge_list:
+            self.add_edge(e[0], e[1])
+
+    def draw(self, q_screen):
+        for e in self.shape.edges():
+            l = Line(to_coord(e[0]), to_coord(e[1])).set_color(BLUE)
+            self.shape.edges[e]['q'].add(l)
+            q_screen.add(l)
+
+    def point_inside(self, p):
+        x = p[0, 0]
+        y = p[1, 0]
+        n = 0
+        for e in self.shape.edges():
+            if e[0][0] < x < e[1][0] or e[1][0] < x < e[0][0]:
+                yp = (x - e[0][0]) * (e[1][1] - e[0][1]) / (e[1][0] - e[0][0]) + e[0][1]
+                if yp < y:
+                    n += 1
+        return n % 2 != 0
+
+    def get_intersection(self, p1, p2):
+        pass
+
+
 class Point:
     def __init__(self, x, y):
         self.id = (x, y)
@@ -12,7 +53,7 @@ class Point:
         self.mass = np.random.uniform(1, 2, 1)[0]
 
     def set_force_gravity(self):
-        self.force[1, 0] = self.force[1, 0] - self.mass * 9.8
+        self.force[0, 0] = self.force[0, 0] - self.mass * 0.3
 
     def set_velocity(self, dt):
         self.velocity = self.velocity + self.force * dt / self.mass
@@ -28,6 +69,10 @@ class Point:
         unit_y = np.array([0, 1], dtype='float64').reshape((2, 1))
         self.force[0, 0] = self.force[0, 0] + np.dot(np.transpose(unit_x), fd) * f
         self.force[1, 0] = self.force[1, 0] + np.dot(np.transpose(unit_y), fd) * f
+
+    def get_next_position(self, dt):
+        v = self.velocity + self.force * dt / self.mass
+        return self.position + v * dt
 
 
 class Spring:
@@ -66,15 +111,20 @@ class SoftRectangle:
                     if n[0] >= 0 and n[1] >= 0:
                         self.g.add_edge((i, j), n, s=Spring(self.g.nodes[(i, j)]['p'], self.g.nodes[n]['p']))
 
-    def step(self, dt):
+    def step(self):
         for n in self.g.nodes():
             self.g.nodes[n]['p'].reset_force()
-            # self.g.nodes[n]['p'].set_force_gravity()
+            self.g.nodes[n]['p'].set_force_gravity()
         for e in self.g.edges():
             self.g.edges[e]['s'].set_force_spring()
+
+    def update_positions(self, dt, p):
         for n in self.g.nodes():
-            self.g.nodes[n]['p'].set_velocity(dt)
-            self.g.nodes[n]['p'].set_position(dt)
+            if p.point_inside(self.g.nodes[n]['p'].get_next_position(dt)):
+                pass
+            else:
+                self.g.nodes[n]['p'].set_velocity(dt)
+                self.g.nodes[n]['p'].set_position(dt)
 
     def move_atom(self, n, d):
         self.g.nodes[n]['p'].position += d
@@ -101,6 +151,11 @@ class SoftBody(Scene):
         for n in s.g.nodes():
             self.add(s.g.nodes[n]['c'])
         s.move_anim(self)
+        p = Polygon()
+        p.add_edges([
+            [(-1, -1), (-2, -2)], [(-1, -1), (-3, 3)], [(-2, -2), (-4, 2)], [(-3, 3), (-4, 2)]
+        ])
+        p.draw(self)
         self.wait(1)
 
         s.move_atom((0, 0), np.array([-0.5, 0], dtype='float64').reshape((2, 1)))
@@ -109,7 +164,8 @@ class SoftBody(Scene):
         self.wait(1)
 
         dt = 0.05
-        for _ in range(700):
-            s.step(dt)
+        for _ in range(150):
+            s.step()
+            s.update_positions(dt, p)
             s.move_anim(self)
             self.wait(2 * dt)
